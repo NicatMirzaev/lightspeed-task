3e01e482220971642e711c9ea78c31ed9f41ed9f
nijadmirzoevi <nicatmirzoev111@gmail.com>
2024-04-17 13:51:39 +0400

    init

---
+++ server/app.ts
@@ -0,0 +1,40 @@
+import express, {
+  Express,
+  Request,
+  Response,
+} from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+app.use(cookieParser());
+app.use(
+  express.static(
+    path.join(__dirname, "public")
+  )
+);
+
+app.get(
+  "/",
+  (req: Request, res: Response) => {
+    res.send(
+      "Express + TypeScript Server"
+    );
+  }
+);
+
+app.listen(port, () => {
+  console.log(
+    `[server]: Server is running at http://localhost:${port}`
+  );
+});
