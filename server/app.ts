0b3d89b16b75793da6dcaf08389ded922d13410e
nijadmirzoevi <nicatmirzoev111@gmail.com>
2024-04-22 00:39:53 +0400

    added fetch products from api

--- server/app.ts
+++ server/app.ts
@@ -1,11 +1,8 @@
-import express, {
-  Express,
-  Request,
-  Response,
-} from "express";
+import express, { Express, Request, Response } from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
+import cors from "cors";
 
 import "dotenv/config";
 
@@ -15,6 +12,7 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.use(logger("dev"));
+app.use(cors());
 app.use(express.json());
 app.use(
   express.urlencoded({
@@ -22,22 +20,14 @@ app.use(
   })
 );
 app.use(cookieParser());
-app.use(
-  express.static(
-    path.join(__dirname, "public")
-  )
-);
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (_, res: Response) => {
-  res.send(
-    "Express + TypeScript Server"
-  );
+  res.send("Express + TypeScript Server");
 });
 
 app.use("/api", apiRouter);
 
 app.listen(port, () => {
-  console.log(
-    `[server]: Server is running at http://localhost:${port}`
-  );
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
