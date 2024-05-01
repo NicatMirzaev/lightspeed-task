27900432118240734157187c05052d9f32521581
nijadmirzoevi <nicatmirzoev111@gmail.com>
2024-05-01 21:31:42 +0400

    home page done

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,9 +1,34 @@
 <template>
-  <h1>Home</h1>
+  <div class="landing">
+    <h1>Welcome to Lightspeed Test Task!</h1>
+    <p>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the
+      1500s
+    </p>
+    <button class="btn btn-primary" @click="goStore">Browse store</button>
+  </div>
 </template>
 
 <script>
 export default {
   name: "home-view",
+  methods: {
+    goStore() {
+      this.$router.push("/store");
+    },
+  },
 };
 </script>
+
+<style>
+.landing {
+  text-align: center;
+  max-width: 800px;
+  margin-left: auto;
+  margin-right: auto;
+}
+.landing p {
+  color: #9f9f9f;
+}
+</style>
